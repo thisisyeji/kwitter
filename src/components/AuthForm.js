f8316705056474cbcb283f9b91da7278e64e8552
yjkim0109 <yjkkim0109@gmail.com>
2022-10-28 17:15:45 +0900

    Feat: 이메일 가입 및 로그인 기능

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,35 +1,70 @@
 import React, { useState } from 'react';
+import { authService } from 'fbase';
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 const AuthForm = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [password2, setPassword2] = useState('');
+	const [error, setError] = useState('');
 	const [newMember, setNewMember] = useState(false);
+
+	const authError = {
+		'auth/weak-password': '비밀번호를 6글자 이상 입력하세요.',
+		'auth/email-already-in-use': '이미 가입된 이메일입니다.',
+		'auth/invalid-email': '유효하지 않은 이메일입니다.',
+		'auth/wrong-password': '잘못된 비밀번호 입니다.',
+		'auth/user-not-found': '해당 계정을 찾을 수 없습니다.',
+	};
 
 	const onChange = (e) => {
 		const {
 			target: { name, value },
 		} = e;
 		if (name === 'email') {
-			setEmail(value);
+			setEmail(value.trim());
 		} else if (name === 'password') {
-			setPassword(value);
+			setPassword(value.trim());
 		} else if (name === 'password2') {
-			setPassword2(value);
+			setPassword2(value.trim());
 		}
 	};
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
-		// try {
-		// let data;
-		// if(newMember) {
-		// data = await createUserWithEmailAndPassword(
+		if (newMember) {
+			if (!password2 || password !== password2)
+				return setError('비밀번호를 동일하게 입력하세요.');
+		}
 
-		//   )
-		// }
-		// }
+		try {
+			let data;
+			if (newMember) {
+				// create account
+				data = await createUserWithEmailAndPassword(
+					authService,
+					email,
+					password
+				);
+			} else {
+				// log in
+				data = await signInWithEmailAndPassword(authService, email, password);
+			}
+			console.log(data);
+		} catch (e) {
+			const error = e.code;
+			console.log(error);
+			if (error) {
+				const errorValue = authError[error];
+				setError(errorValue);
+			}
+		}
 	};
+
+	const toggleAccount = () => setNewMember((prev) => !prev);
 
 	return (
 		<>
@@ -61,9 +96,14 @@ const AuthForm = () => {
 					/>
 				)}
 				<button type='submit'>{newMember ? '가입하기' : '로그인'}</button>
+				<p>{error}</p>
 			</form>
-			<p>{newMember ? '이미 크위터에 가입하셨나요?' : '계정이 없으신가요?'}</p>
-			<span>{newMember ? '로그인' : '가입하기'}</span>
+			<div>
+				<span>
+					{newMember ? '이미 크위터에 가입하셨나요?' : '계정이 없으신가요?'}
+				</span>
+				<span onClick={toggleAccount}>{newMember ? '로그인' : '가입하기'}</span>
+			</div>
 		</>
 	);
 };
