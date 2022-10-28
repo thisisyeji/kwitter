b9138d4e852dacb46c21f071701655a8db62f2d2
yjkim0109 <yjkkim0109@gmail.com>
2022-10-28 14:55:16 +0900

    Feat: 기본 폴더 구조설정 및 Router 추가

---
+++ src/components/Routers.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../routes/Home';
+import Auth from '../routes/Auth';
+import Profile from '../routes/Profile';
+
+const Routers = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	return (
+		<Router>
+			<Switch>
+				{isLoggedIn ? (
+					<>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/profile' component={Profile} />
+					</>
+				) : (
+					<Route exact path='/' component={Auth} />
+				)}
+			</Switch>
+		</Router>
+	);
+};
+
+export default Routers;
