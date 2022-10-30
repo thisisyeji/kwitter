6e65a029d5955349d3eaa54697748380b7eb02b1
yjkim0109 <yjkkim0109@gmail.com>
2022-10-30 23:49:49 +0900

    Feat: storage 사진 업로드/불러오기 기능

--- src/components/Write.js
+++ src/components/Write.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
-import { dbService } from 'fbase';
+import { dbService, storageService } from 'fbase';
+import { ref, uploadString, getDownloadURL } from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
 
 const Write = ({ userObj }) => {
 	const [kweet, setKweet] = useState('');
 	const [active, setActive] = useState('');
+	const [attachment, setAttachment] = useState('');
+	const fileInput = useRef();
 
 	const onChange = (e) => {
 		const {
@@ -17,6 +21,17 @@ const Write = ({ userObj }) => {
 	const onSubmit = async (e) => {
 		e.preventDefault();
 
+		let attachmentUrl = '';
+		if (attachment !== '') {
+			const attachmentRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+			const response = await uploadString(
+				attachmentRef,
+				attachment,
+				'data_url'
+			);
+			attachmentUrl = await getDownloadURL(response.ref);
+		}
+
 		const options = {
 			year: 'numeric',
 			month: 'long',
@@ -24,17 +39,44 @@ const Write = ({ userObj }) => {
 			hour: 'numeric',
 			minute: 'numeric',
 		};
+
 		let createdDate = new Date().toLocaleString('ko-KR', options);
-		await addDoc(collection(dbService, 'kweets'), {
+
+		const kweetObj = {
 			text: kweet,
 			createdAt: Date.now(),
 			createdDate: createdDate,
 			creatorId: userObj.uid,
 			creatorName: userObj.displayName,
 			profileImg: userObj.photoURL,
-		});
+			attachmentUrl,
+		};
+
+		await addDoc(collection(dbService, 'kweets'), kweetObj);
 		setKweet('');
 		setActive('');
+		setAttachment('');
+		fileInput.current.value = '';
+	};
+
+	const onFileChange = (e) => {
+		const {
+			target: { files },
+		} = e;
+		const theFile = files[0];
+		const reader = new FileReader();
+		reader.onloadend = (finishedEvent) => {
+			const {
+				currentTarget: { result },
+			} = finishedEvent;
+			setAttachment(result);
+		};
+		reader.readAsDataURL(theFile);
+	};
+
+	const onClearAttachment = () => {
+		setAttachment('');
+		fileInput.current.value = '';
 	};
 
 	return (
@@ -46,7 +88,19 @@ const Write = ({ userObj }) => {
 				value={kweet}
 				onChange={onChange}
 			/>
+			<input
+				type='file'
+				accept='image/*'
+				onChange={onFileChange}
+				ref={fileInput}
+			/>
 			<input type='submit' value='크윗하기' disabled={!active} />
+			{attachment && (
+				<div>
+					<img src={attachment} alt='attachment' width='50px' height='50px' />
+					<button onClick={onClearAttachment}>X</button>
+				</div>
+			)}
 		</form>
 	);
 };
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { signOut } from 'firebase/auth';
 import { authService } from 'fbase';
 import { useHistory } from 'react-router-dom';
+import { dbService } from 'fbase';
+import {
+	collection,
+	query,
+	where,
+	onSnapshot,
+	getDoc,
+} from 'firebase/firestore';
 
 const Profile = () => {
 	const history = useHistory();
+
+	// const q = query(collection(dbService, 'kweets').where(user.uid));
 
 	const onLogOutClick = () => {
 		signOut(authService);
