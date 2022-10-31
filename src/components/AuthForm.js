a4ee1302474393f2dce704f6c72314483cad0420
yjkim0109 <yjkkim0109@gmail.com>
2022-10-31 19:34:08 +0900

    Design: Auth, AuthForm 컴포넌트 스타일링

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -4,6 +4,66 @@ import {
 	createUserWithEmailAndPassword,
 	signInWithEmailAndPassword,
 } from 'firebase/auth';
+import styled from 'styled-components';
+
+const FormBox = styled.form`
+	max-width: 500px;
+	display: flex;
+	flex-direction: column;
+	gap: 5px;
+
+	input {
+		padding: 10px;
+		outline: none;
+
+		&:focus {
+			border: 2px solid rgb(29, 155, 240);
+		}
+	}
+
+	input[type='submit'] {
+		font-size: 18px;
+		font-weight: 600;
+		margin: 15px 0 40px;
+		border: 1px solid rgb(29, 155, 240);
+		border-radius: 30px;
+		background: rgb(29, 155, 240);
+		color: #fff;
+		cursor: pointer;
+		transition: 0.5s;
+
+		&:hover {
+			box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+		}
+	}
+
+	.error {
+		font-size: 14px;
+		font-weight: 600;
+		color: hotpink;
+		margin-top: 5px;
+	}
+`;
+
+const ToggleBox = styled.div`
+	max-width: 500px;
+	color: #777;
+	text-align: center;
+
+	.toggle {
+		color: rgb(29, 155, 240);
+		font-weight: 700;
+		border-bottom: 2px solid transparent;
+		margin-left: 10px;
+		margin-bottom: 20px;
+		cursor: pointer;
+		transition: 0.5s;
+
+		&:hover {
+			border-bottom: 2px solid rgb(29, 155, 240);
+		}
+	}
+`;
 
 const AuthForm = () => {
 	const [email, setEmail] = useState('');
@@ -56,7 +116,6 @@ const AuthForm = () => {
 			console.log(data);
 		} catch (e) {
 			const error = e.code;
-			console.log(error);
 			if (error) {
 				const errorValue = authError[error];
 				setError(errorValue);
@@ -64,11 +123,17 @@ const AuthForm = () => {
 		}
 	};
 
-	const toggleAccount = () => setNewMember((prev) => !prev);
+	const toggleAccount = () => {
+		setNewMember((prev) => !prev);
+		setEmail('');
+		setPassword('');
+		setPassword2('');
+		setError('');
+	};
 
 	return (
 		<>
-			<form onSubmit={onSubmit}>
+			<FormBox onSubmit={onSubmit}>
 				<input
 					type='email'
 					name='email'
@@ -95,15 +160,18 @@ const AuthForm = () => {
 						onChange={onChange}
 					/>
 				)}
+				<p className='error'>{error}</p>
 				<input type='submit' value={newMember ? '가입하기' : '로그인'} />
-				<p>{error}</p>
-			</form>
-			<div>
+			</FormBox>
+
+			<ToggleBox>
 				<span>
 					{newMember ? '이미 크위터에 가입하셨나요?' : '계정이 없으신가요?'}
 				</span>
-				<span onClick={toggleAccount}>{newMember ? '로그인' : '가입하기'}</span>
-			</div>
+				<span className='toggle' onClick={toggleAccount}>
+					{newMember ? '로그인' : '가입하기'}
+				</span>
+			</ToggleBox>
 		</>
 	);
 };
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -6,6 +6,68 @@ import {
 	GithubAuthProvider,
 } from 'firebase/auth';
 import { authService } from 'fbase';
+import { FaTwitter } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const AuthWrap = styled.section`
+	width: 100vw;
+	height: 100vh;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	.left {
+		width: 50%;
+		height: 100%;
+		color: #fff;
+		font-size: 10rem;
+		background: rgb(29, 155, 240);
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.right {
+		width: 50%;
+		height: 100%;
+		margin: 0 50px;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+
+		h1 {
+			font-size: 4vw;
+			font-weight: 700;
+			margin-bottom: 30px;
+		}
+
+		.accounts {
+			max-width: 500px;
+			margin-top: 30px;
+
+			display: flex;
+			flex-direction: column;
+			gap: 10px;
+
+			button {
+				border: 1px solid #efefef;
+				border-radius: 30px;
+				background: none;
+				padding: 10px;
+				cursor: pointer;
+				transition: 0.5s;
+
+				&:hover {
+					border: 1px solid rgb(29, 155, 240);
+					color: rgb(29, 155, 240);
+				}
+			}
+		}
+	}
+`;
 
 const Auth = () => {
 	const onSocialClick = async (e) => {
@@ -24,17 +86,26 @@ const Auth = () => {
 		console.log(data);
 	};
 	return (
-		<div>
-			<AuthForm />
-			<div>
-				<button name='google' onClick={onSocialClick}>
-					Google 계정으로 계속하기
-				</button>
-				<button name='github' onClick={onSocialClick}>
-					Github 계정으로 계속하기
-				</button>
+		<AuthWrap>
+			<div className='left'>
+				<FaTwitter />
 			</div>
-		</div>
+
+			<div className='right'>
+				<h1>크위터에 로그인 하기</h1>
+
+				<AuthForm />
+
+				<div className='accounts'>
+					<button name='google' onClick={onSocialClick}>
+						Google 계정으로 계속하기
+					</button>
+					<button name='github' onClick={onSocialClick}>
+						Github 계정으로 계속하기
+					</button>
+				</div>
+			</div>
+		</AuthWrap>
 	);
 };
 
